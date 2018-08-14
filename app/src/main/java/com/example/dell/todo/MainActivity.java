17d9fbd071291167c903ab140bdf8770aa89345d
aanshi <aanshiverma03@gmail.com>
2018-08-14 16:57:35 +0530

    3

--- app/src/main/java/com/example/dell/todo/MainActivity.java
+++ app/src/main/java/com/example/dell/todo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dell.todo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button click=(Button)findViewById(R.id.button2);
+        click.setOnClickListener(
+                new  Button.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Intent myIntent = new Intent(MainActivity.this,
+                                TodoPage.class);
+                        startActivity(myIntent);
+                    }
+                }
+        );
+
     }
 }
